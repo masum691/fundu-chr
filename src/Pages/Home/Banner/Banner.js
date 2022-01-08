0a2ea8474e9a918a704e45c454adedb05fdc99c6
masum691 <masumhu691@gmail.com>
2022-01-08 16:58:56 +0600

    top and main nav

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import MainNav from '../../Shared/Navbar/MainNav/MainNav';
+import TopNav from '../../Shared/Navbar/TopNav/TopNav';
+import './Banner.css'
+import manImg from '../../../Images/man.png'
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='banner-container pt-3'>
+            <TopNav></TopNav>
+            <MainNav></MainNav>
+            <div className='width-container'>
+                <div className='row align-items-center pt-3'>
+                    <div className='col-sm-12 col-lg-6'>
+                        <h6>Our Missions From Jesus</h6>
+                        <h1>United Churches People Prayers</h1>
+                        <div>
+                            <button>GET STARTED</button>
+                            <button>CAUSES</button>
+                        </div>
+                    </div>
+                    <div className='col-sm-12 col-lg-6'>
+                        <img className='man-img' src={manImg} alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/TopNav/TopNav.js
+++ src/Pages/Shared/Navbar/TopNav/TopNav.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 const TopNav = () => {
     return (
-        <div>
-            
+        <div className='width-container'>
+            <div className='mb-3'>
+            <div className='d-md-flex d-lg-flex justify-content-between'>
+                <div>
+                    <i class="fab fa-facebook-f me-3"></i>
+                    <i class="fab fa-twitter me-3"></i>
+                    <i class="fab fa-behance me-3"></i>
+                    <i class="fab fa-youtube me-3"></i>
+                    <i class="fab fa-pinterest-p me-3"></i>
+                </div>
+                <div>
+                <i class="fas fa-lock me-1"></i>
+                <span>Signup</span>
+                <span>/</span>
+                <span>Login</span>
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
