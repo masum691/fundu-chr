8fea82f89ae692e19f507916d80a10664ccc928a
masum691 <masumhu691@gmail.com>
2022-01-08 19:30:26 +0600

    done getInvoled component

--- src/Pages/Home/GetInvoled/GetInvoled.js
+++ src/Pages/Home/GetInvoled/GetInvoled.js
@@ -1,9 +1,85 @@
 import React from 'react';
+import './GetInvoled.css'
+import event from '../../../Images/word2.png'
 
 const GetInvoled = () => {
     return (
-        <div>
-            
+        <div className='get-involed-container'>
+            <div className='width-container'>
+                {/* <div style={{ backgroundImage: `url(${event})` }}>
+
+                </div> */}
+                <div>
+                    <div className='upcoming-events mb-5'>
+                        <h5 className='text-center'>Upcoming Events</h5>
+                        <h1 className='text-center'>Enjoy & Get Involed <br /> With Us</h1>
+                    </div>
+                </div>
+                <div className='row programe align-items-center mt-3'>
+                    <div className='col-sm-12 col-lg-3'>
+                        <div className=' programe-1'>
+                            <h1>30</h1>
+                            <p>Jun</p>
+                        </div>
+                    </div>
+                    <div className='col-sm-12 col-lg-6 programe-2'>
+                        <p><i class="fas fa-map-marker-alt"></i> Limoda, 16/A, New York, USA</p>
+                        <h3>Lafanga Little Bird Program</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab consectetur impedit minima quod atque.</p>
+                    </div>
+                    <div className='col-sm-12 col-lg-3 programe-3 text-lg-center'>
+                        <button className='find-more'>Find more</button>
+                    </div>
+                </div>
+                <div className='row programe align-items-center mt-3'>
+                    <div className='col-sm-12 col-lg-3'>
+                        <div className=' programe-1'>
+                            <h1>30</h1>
+                            <p>Jun</p>
+                        </div>
+                    </div>
+                    <div className='col-sm-12 col-lg-6 programe-2'>
+                        <p><i class="fas fa-map-marker-alt"></i> Limoda, 16/A, New York, USA</p>
+                        <h3>Lafanga Little Bird Program</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab consectetur impedit minima quod atque.</p>
+                    </div>
+                    <div className='col-sm-12 col-lg-3 programe-3 text-lg-center'>
+                        <button className='find-more'>Find more</button>
+                    </div>
+                </div>
+                <div className='row programe align-items-center mt-3'>
+                    <div className='col-sm-12 col-lg-3'>
+                        <div className=' programe-1'>
+                            <h1>30</h1>
+                            <p>Jun</p>
+                        </div>
+                    </div>
+                    <div className='col-sm-12 col-lg-6 programe-2'>
+                        <p><i class="fas fa-map-marker-alt"></i> Limoda, 16/A, New York, USA</p>
+                        <h3>Lafanga Little Bird Program</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab consectetur impedit minima quod atque.</p>
+                    </div>
+                    <div className='col-sm-12 col-lg-3 programe-3 text-lg-center'>
+                        <button className='find-more'>Find more</button>
+                    </div>
+                </div>
+                <div className='row programe align-items-center mt-3'>
+                    <div className='col-sm-12 col-lg-3'>
+                        <div className=' programe-1'>
+                            <h1>30</h1>
+                            <p>Jun</p>
+                        </div>
+                    </div>
+                    <div className='col-sm-12 col-lg-6 programe-2'>
+                        <p><i class="fas fa-map-marker-alt"></i> Limoda, 16/A, New York, USA</p>
+                        <h3>Lafanga Little Bird Program</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab consectetur impedit minima quod atque.</p>
+                    </div>
+                    <div className='col-sm-12 col-lg-3 programe-3 text-lg-center'>
+                        <button className='find-more'>Find more</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutChurches from '../AboutChurches/AboutChurches';
 import Banner from '../Banner/Banner';
+import GetInvoled from '../GetInvoled/GetInvoled';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <AboutChurches></AboutChurches>
+            <GetInvoled></GetInvoled>
         </div>
     );
 };
